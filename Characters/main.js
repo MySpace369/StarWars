024ad3dcb6b3731fec981ce6b8e367ce8568ae24
Adrienne <10910835@my.uvu.edu>
2021-03-16 09:52:18 -0600

    Tried to add css properties

--- Characters/main.js
+++ Characters/main.js
@@ -1,27 +1,55 @@
-import { people } from '../data/people.js'
+import { people } from './data/people.js'
 
-const mainElement = document.querySelector('#main')
+const Title = people[0].title
+console.log (Title)
 
-people.forEach((person. i) => {
-    console.log(person.name)
-    const charFigure = document.createElement('figure')
-    const charImg = document.createElement('img')
-    charImg.src = 'https://starwars-visualguide.com/#/characters?${charNum + 1}.jpg'
-    const charCaption = document.createElement('figcaption')
-    charCaption.textContent = person.name
+let peoplelist = document.getElementById('peoplelist')
 
-    charFigure.appendChild(charImg)
-    charFigure.appendChild(charCaption)
+console.log (peoplelist)
 
-    mainElement.appendChild(charFigure)
+for (let i = 0; i < people.length; i++) {
 
+    let titletext = document.createElement('p')
+    titletext.innerText = people[i].title
+    let peopleid = people[i]['character_id']
+    let image = document.createElement('img')
+    image.src = `https://starwars-visualguide.com/#/characters?page=1${peopleid}.jpg`
+    let figcaption = document.createElement('figcaption')
 
-})
+    peoplelist.appendChild(titletext)
+    peoplelist.appendChild(image)
+}
 
 function getLastNumber(url) {
     let end = url.lastIndexOf('/')
-    let start = end - 2
-    if (url.charAt(start) == '/')
-
     return url.charAt(end - 1)
 }
+
+
+// import { people } from '../data/people.js'
+
+// const mainElement = document.querySelector('#main')
+
+// people.forEach((person. i) => {
+//     console.log(person.name)
+//     const charFigure = document.createElement('figure')
+//     const charImg = document.createElement('img')
+//     charImg.src = 'https://starwars-visualguide.com/#/characters?${charNum + 1}.jpg'
+//     const charCaption = document.createElement('figcaption')
+//     charCaption.textContent = person.name
+
+//     charFigure.appendChild(charImg)
+//     charFigure.appendChild(charCaption)
+
+//     mainElement.appendChild(charFigure)
+
+
+// })
+
+// function getLastNumber(url) {
+//     let end = url.lastIndexOf('/')
+//     let start = end - 2
+//     if (url.charAt(start) == '/')
+
+//     return url.charAt(end - 1)
+// }
